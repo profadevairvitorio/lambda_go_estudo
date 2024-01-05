4ee8ca2653e2d494be3cc7982f040c92b37642a2
profadevairvitorio <profadevairvitorio@gmail.com>
2024-01-05 01:00:10 -0300

    chore: create first lambda AWS in GO

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lambda_go_estudo
+
+go 1.21.4
+
+require github.com/aws/aws-lambda-go v1.43.0 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	nome := event.QueryStringParameters["Test"]
+	mensagem := fmt.Sprintf("teste, recebi o %s.", nome)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       mensagem,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
